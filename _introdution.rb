47987dfe608819973f7109f060dfe5be8cd0d209
Érik Freitas Morais <74334533+erikfritas@users.noreply.github.com>
2021-09-26 22:55:15 -0300

    intruduction

---
+++ _introdution.rb
@@ -0,0 +1,26 @@
+# Introdution
+
+def title(text, space=15, chars='-=')
+    puts "#{chars*space} #{text} #{chars*space}"
+end
+
+def description(text, space=84, chars='-')
+    puts chars*space
+    puts text
+    puts chars*space
+end
+
+title("Hello, I'm erikfritas")
+
+puts
+
+description("In this course I will teach you step by step how to program in ruby ​​from 0, from making a simple application in the terminal to a fullstack site in ruby, using the MVC pattern")
+
+
+puts "  _________"
+puts " /__||_||__\\"
+puts " \\ _______ /"
+puts " | |     | |"
+puts " | |     | |"
+puts " |-|     |-|"
+
